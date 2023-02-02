12177de3a72e917396c124d293e9618cce372ab0
Skyler Brandt <skylerak90704@gmail.com>
2023-02-01 16:48:52 -0900

    finish game

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
-function getComputerChoice() {}
+function getComputerChoice() {
+  const options = ["rock", "paper", "scissors"];
+  return options[Math.floor(Math.random() * options.length)];
+}
 
-function playRound(playerSelection, computerSelection) {}
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection === "rock" && computerSelection === "scissors") {
+    return "You win!, rock beats scissors";
+  } else if (playerSelection === "paper" && computerSelection === "rock") {
+    return "You win!, paper beats rock";
+  } else if (playerSelection === "scissors" && computerSelection === "paper") {
+    return "You win!, scissors beats paper";
+  } else {
+    return `You lose!, ${computerSelection} beats ${playerSelection}`;
+  }
+}
 
-function playGame() {}
+function playGame(rounds) {
+  for (let i = 0; i < rounds; ++i) {
+    const playerChoice = prompt("Rock, paper, scissors");
+    console.log(playRound(playerChoice, getComputerChoice()));
+  }
+}
